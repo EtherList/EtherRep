bd600e489c18c974c64b0501c360071c6aff814c
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-08-10 20:24:29 -0700

    feat: Add EtherRep#deployBasicContract()

--- contracts/EtherRep.sol
+++ contracts/EtherRep.sol
@@ -1,5 +1,8 @@
+import "./BasicContract.sol";
+
 contract EtherRep {
   event Rating(address from, address to, uint repPoints, uint newTotalRating);
+  event DeployBasicContract(address contractt);
 
   mapping (address => uint) numRatings;
   mapping (address => uint) reputation;
@@ -29,5 +32,11 @@ contract EtherRep {
     reputation[who] -= repPoints;
 
     Rating(msg.sender, who, repPoints, reputation[who]);
+  }
+
+  function deployBasicContract(address seller, address buyer) returns (address) {
+    address c = address(new BasicContract(seller, buyer));
+    DeployBasicContract(c);
+    return c;
   }
 }
