da35e480c6492788ed784c5a51b515efc3efea76
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-08-14 15:50:33 -0700

    build: deploy to `testrpc -d`

--- app/index.js
+++ app/index.js
@@ -3,13 +3,13 @@ var account;
 var balance;
 
 window.onload = function() {
-  web3.eth.getAccounts((err, accs) => {
+  web3.eth.getAccounts((err, accts) => {
     if (err !== null) {
       alert('There was an error fetching your accounts');
       return;
     }
 
-    if (accs.length === 0) {
+    if (accts.length === 0) {
       alert('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.');
       return;
     }
