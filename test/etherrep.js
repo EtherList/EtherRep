d7014a061676c20f81a8de2cf5330a58e78c2004
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-08-20 17:23:44 -0700

    feat: Refactor EtherRep and test

--- test/etherrep.js
+++ test/etherrep.js
@@ -1,70 +1,95 @@
-let Promise = require('bluebird');
+const utils = require('./utils');
 
-function repeat(action, nTimes) {
-  let p = action();
-  for (let i = 1; i < nTimes; i++) {
-    p.then(action);
-  }
-  return p;
-}
-
-Promise.prototype.repeat = function(action, nTimes) {
-  let p = this;
-  for (let i = 0; i < nTimes; i++) {
-    p = p.then(action);
-  }
-  return p;
-};
-
-contract('Formerly EtherRep', function(accounts) {
+contract('EtherRep', function(accounts) {
   let rep;
+  let zero  = accounts[0];
+  let alice = accounts[1];
+  let factory = accounts[2];
+
+  let getAliceRep = () => rep.reputation.call(alice);
+  let increaseAliceRep = (points) => () => rep.increaseReputation(alice, points, {from: factory});
+  let decreaseAliceRep = (points) => () => rep.decreaseReputation(alice, points, {from: factory});
+  let setFactoryPerms = (maxInc, maxDec) => () => rep.setPermissionLimits(factory, maxInc, maxDec, {from: zero});
+  let getFactoryMaxInc = () => rep.maxIncrement.call(factory);
+  let assertValueOf = (expected) => (bigInt) => assert.equal(bigInt.valueOf(), expected);
 
   beforeEach(function() {
     rep = EtherRep.deployed();
+
+    return setFactoryPerms(1000, 1000)()
+    .then(decreaseAliceRep(1000))
+    .then(setFactoryPerms(0, 0));
   });
 
-  it('defaults to zero reputation', function() {
-    return rep.getReputation.call(accounts[0]).then(reputation => {
-      assert.equal(reputation.valueOf(), 0, "default reputation wasn't 0");
-    });
+  it('allows the owner to set permissions', function() {
+    return setFactoryPerms(10, 5)()
+    .then(getFactoryMaxInc)
+    .then(assertValueOf(10));
   });
 
-  it('defaults to zero ratings', function() {
-    return rep.getNumRatings.call(accounts[0])
-      .then(numRatings => {
-        assert.equal(numRatings.valueOf(), 0, "defualt number of ratings wasn't 0");
-      });
+  it('does not allow others to set permissions', function() {
+    return rep.setPermissionLimits(factory, 10, 5, {from: alice})
+    .then(getFactoryMaxInc)
+    .then(assertValueOf(0));
   });
 
-  it('gets the number of ratings', function() {
-    return rep.increaseRep(accounts[0])
-      .then(() => rep.decreaseRep(accounts[0])
-        .then(rep.getNumRatings.call.bind(rep, accounts[0]))
-        .then(numRatings => {
-          assert.equal(numRatings.valueOf(), 2, 'number of ratings should be 2')
-        }));
+  it('increases reputation when allowed', function() {
+    return setFactoryPerms(10, 5)()
+    .then(increaseAliceRep(10))
+    .then(getAliceRep)
+    .then(assertValueOf(10));
   });
 
-  it('increases reputation', function() {
-    return rep.getReputation.call(accounts[0])
-      .then(curReputation => {
-        return rep.increaseRep(accounts[0])
-          .then(rep.getReputation.call.bind(rep, accounts[0]))
-          .then(newReputation => assert.equal(newReputation.valueOf(), Number(curReputation + 10), 'did not increase reputation'));
-      });
+  it('does not increase reputation more than allowed', function() {
+    return setFactoryPerms(10, 5)()
+    .then(increaseAliceRep(11))
+    .then(getAliceRep)
+    .then(assertValueOf(10));
   });
 
-  it('decreases reputation by 10', function() {
-    return repeat(() => rep.increaseRep(accounts[1]), 3)
-      .then(() => rep.decreaseRep(accounts[1]))
-      .then(rep.getReputation.call.bind(rep, accounts[1]))
-      .then(reputation => assert.equal(reputation.valueOf(), 20, 'did not decrease reputation by 10'));
+  it('does not increase reputation when not allowed because factory is not approved', function() {
+    return rep.increaseReputation(alice, 10, {from: accounts[4]})
+    .then(getAliceRep)
+    .then(assertValueOf(0));
   });
 
-  it('decreases reputation by percentage', function() {
-    return repeat(() => rep.increaseRep(accounts[2]), 20)
-      .then(() => rep.decreaseRep(accounts[2]))
-      .then(rep.getReputation.call.bind(rep, accounts[2]))
-      .then(newReputation => assert.equal(newReputation.valueOf(), 180, 'did not decrease reputation by percentage'));
+  it('does not increase reputation by negative numbers', function() {
+    // negative numbers become huge positive numbers, which are capped
+    return setFactoryPerms(10, 5)()
+    .then(increaseAliceRep(10))
+    .then(increaseAliceRep(-5))
+    .then(getAliceRep)
+    .then(assertValueOf(20));
   });
+
+  it('accepts strings gracefully', function() {
+    return setFactoryPerms(10, 5)()
+    .then(increaseAliceRep(10))
+    .then(increaseAliceRep('5'))
+    .then(getAliceRep)
+    .then(assertValueOf(15));
+  });
+
+  it('decreases reputation when allowed', function() {
+    return setFactoryPerms(10, 5)()
+    .then(increaseAliceRep(10))
+    .then(decreaseAliceRep(5))
+    .then(getAliceRep)
+    .then(assertValueOf(5));
+  });
+
+  it('does not decrease reputation more than allowed', function() {
+    return setFactoryPerms(10, 5)()
+    .then(increaseAliceRep(10))
+    .then(decreaseAliceRep(6))
+    .then(getAliceRep)
+    .then(assertValueOf(5));
+  });
+
+  it('does not decrease reputation when not allowed because factory is not approved', function() {
+    return rep.decreaseReputation(alice, 10, {from: accounts[4]})
+    .then(getAliceRep)
+    .then(assertValueOf(0));
+  });
+
 });
