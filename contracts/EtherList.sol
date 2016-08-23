5f206672f40283918acdd962203f75af8028f73a
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-08-22 20:04:52 -0700

    feat: complete EtherList functionality and roll BasicContract into it

--- contracts/EtherList.sol
+++ contracts/EtherList.sol
@@ -1,5 +1,4 @@
 import "./EtherRep.sol";
-import "./BasicContract.sol";
 
 contract EtherList {
   event CreateContract(address newContractAddr);
@@ -26,8 +25,60 @@ contract EtherList {
 
   function completeContract(address contractAddr) {
     // 1. verify sender of transaction (EtherList or contract itself)
-    // 2. mark it completed
-    // 3. boost reputations using erep
+    if (msg.sender == owner) {
+      // fancy stuff
+    } else if (uint(contractStatii[msg.sender]) == 1 && msg.sender == contractAddr) {
+      // 2. mark it completed
+      contractStatii[msg.sender] = ContractStatus.Completed;
+      // 3. boost reputations IF successful
+      //    hurt reputation   IF not successful
+      BasicContract bc = BasicContract(msg.sender);
+      uint status = uint(bc.getStatus());
+      if (status == 2) {
+        EtherRep(erep).increaseReputation(bc.seller(), 10);
+        EtherRep(erep).increaseReputation(bc.buyer(), 10);
+      } else {
+        EtherRep(erep).decreaseReputation(bc.seller(), 5);
+        EtherRep(erep).decreaseReputation(bc.buyer(), 5);
+      }
+    }
+  }
+
+}
+
+contract BasicContract {
+
+  enum Satisfaction { NotYetResponded, Unsatisfied, Satisfied }
+
+  address public elist;
+
+  address public seller;
+  address public buyer;
+
+  mapping (address => Satisfaction) public responses;
+
+  function BasicContract(address sellerAddr, address buyerAddr) {
+    elist = msg.sender;
+    seller = sellerAddr;
+    buyer = buyerAddr;
+  }
+
+  function markCompleted(Satisfaction senderSatisfaction) {
+    // 1. Check that the user has not already responded
+    if (responses[msg.sender] != Satisfaction.NotYetResponded) {
+      return;
+    }
+    // 2. Set user response
+    responses[msg.sender] = senderSatisfaction;
+    // 3. Check whether the contract is completed
+    //    and if it is, let EtherList know
+    if (uint(getStatus()) > 0) {
+      EtherList(elist).completeContract(this);
+    }
+  }
+
+  function getStatus() returns (Satisfaction) {
+    return responses[seller] < responses[buyer] ? responses[seller] : responses[buyer];
   }
 
 }
--- test/etherlist.js
+++ test/etherlist.js
@@ -1,16 +1,155 @@
-contract('EtherList', function(accounts) {
-  let elist;
+contract('BasicContract', function(accounts) {
   let erep;
+  let elist;
+  let bc;
+  let filter;
+  let zero = accounts[0];
+  let alice = accounts[1];
+  let bob   = accounts[2];
+  let contractAddr = null;
 
-  beforeEach(function() {
+  let ContractStatus = {
+    NotEtherList: 0,
+    InProgress: 1,
+    Completed: 2
+  };
+
+  const Satisfaction = {
+    NotYetResponded: 0,
+    Unsatisfied: 1,
+    Satisfied: 2
+  };
+
+  let setFactoryPerms = (maxInc, maxDec) => () => erep.setPermissionLimits(elist.address, maxInc, maxDec, {from: zero});
+  let getRep = (who) => () => erep.reputation.call(who);
+  let markSatisfied = (who) => () => bc.markCompleted(Satisfaction.Satisfied, {from: who});
+  let markUnsatisfied = (who) => () => bc.markCompleted(Satisfaction.Unsatisfied, {from: who});
+  let assertValueOf = (expected) => (bigInt) => assert.equal(bigInt.valueOf(), expected);
+  let wait = (ms) => () => new Promise((resolve) => {setTimeout(() => {resolve(null)}, ms)});
+
+  function assertStatus(expected) {
+    return bc.getStatus.call()
+    .then(assertValueOf(expected));
+  }
+
+  before(function() {
     erep = EtherRep.deployed();
     return EtherList.new(erep.address)
-    .then(instance => { elist = instance; });
+    .then(elistInstance => {
+      elist = elistInstance;
+      filter = elist.CreateContract({});
+      filter.watch((error, log) => {
+        if (error) {
+          console.error('err=', error);
+        } else {
+          contractAddr = log.args.newContractAddr;
+        }
+      });
+    })
+    .then(setFactoryPerms(10, 5));
   });
 
-  it('is deployed with EtherReps address', function() {
-    return elist.erep.call().then(erepAddr => {
-      assert.equal(erepAddr, erep.address);
+  beforeEach(function() {
+    contractAddr = null;
+    elist.createContract(alice, bob);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        bc = BasicContract.at(contractAddr);
+        resolve(null)
+      }, 1000);
+    });
+  });
+
+  after(function() {
+    filter.stopWatching();
+  });
+
+  describe('EtherList', function() {
+    it('is deployed with EtherReps address', function() {
+      return elist.erep.call().then(erepAddr => {
+        assert.equal(erepAddr, erep.address);
+      });
+    });
+
+    it('creates a contract', function() {
+      return elist.createContract(alice, bob)
+      .then(wait(1000))
+      .then(() => {
+        return elist.contractStatii.call(contractAddr)
+      })
+      .then(assertValueOf(ContractStatus.InProgress));
+    });
+
+    it('increases reputation when a contract completes successfully', function() {
+      return elist.createContract(alice, bob)
+      .then(wait(1000))
+      .then(markSatisfied(alice))
+      .then(markSatisfied(bob))
+      .then(getRep(alice))
+      .then(assertValueOf(10))
+      .then(getRep(bob))
+      .then(assertValueOf(10));
+    });
+
+    it('decreases reputation when both parties are unsatisfied', function() {
+      return elist.createContract(alice, bob)
+      .then(wait(1000))
+      .then(markUnsatisfied(alice))
+      .then(markUnsatisfied(bob))
+      .then(getRep(alice))
+      .then(assertValueOf(5))
+      .then(getRep(bob))
+      .then(assertValueOf(5));
+    });
+
+    it('decreases reputation when one of the parties is unsatisfied', function() {
+      return elist.createContract(alice, bob)
+      .then(wait(1000))
+      .then(markSatisfied(alice))
+      .then(markUnsatisfied(bob))
+      .then(getRep(alice))
+      .then(assertValueOf(0))
+      .then(getRep(bob))
+      .then(assertValueOf(0));
+    });
+  });
+
+  describe('BasicContract', function() {
+    it('completes successfully when both parties are satisfied', function() {
+      return markSatisfied(alice)()
+      .then(markSatisfied(bob))
+      .then(assertStatus(Satisfaction.Satisfied));
+    });
+
+    it('completes unsuccessfully if a party marks it so', function() {
+      return markSatisfied(alice)()
+      .then(markUnsatisfied(bob))
+      .then(assertStatus(Satisfaction.Unsatisfied));
+    });
+
+    it('is not completed if one party tries to complete it twice', function() {
+      return markSatisfied(alice)()
+      .then(markSatisfied(alice))
+      .then(assertStatus(Satisfaction.NotYetResponded));
+    });
+
+    it('does not allow satisfaction to change', function() {
+      return markSatisfied(alice)()
+      .then(markUnsatisfied(alice))
+      .then(() => {
+        return bc.responses.call(alice);
+      })
+      .then(assertValueOf(Satisfaction.Satisfied));
+    });
+  });
+
+  
+
+  it('should chill out', function() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(null);
+      }, 10);
     });
   });
 
